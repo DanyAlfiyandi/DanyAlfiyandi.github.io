bda7951c0a546eb8c65a1706ee12a1da2d6b34d0
Daf A.R Channel <danyalfiyandi10.com>
2024-02-17 17:29:25 +0700

    master

--- scripts/main.js
+++ scripts/main.js
@@ -2,14 +2,39 @@
 const navSlide = () => {
     const burger = document.querySelector(".burger");
     const navLists = document.querySelector("nav");
+    const nav = document.querySelector('.nav ul');
+    const navLinks = document.querySelectorAll('.nav ul li');
 
     burger.addEventListener("click", () => {
         navLists.classList.toggle("nav-active");
         burger.classList.toggle("toggle-burger");
     });
+      // Animate Links
+      navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = '';
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+        }
+    });
+
 };
 
 navSlide();
+const scrollLinks = document.querySelectorAll('.nav ul li a');
+
+scrollLinks.forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+
+        const target = document.querySelector(link.getAttribute('href'));
+
+        window.scrollTo({
+            top: target.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
 
 //clear form before unload 
 window.onbeforeunload = () => {
